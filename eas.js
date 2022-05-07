dce5e84dbf0a739953fa984c07a29ce99cac472e
Kasper <kasperbak89@pm.me>
2022-05-07 17:42:33 +0200

    Add hovering with javascript

--- eas.js
+++ eas.js
@@ -1,9 +1,8 @@
 // create and appends el passed num of times
 function createEl(parent, el, num){
     for(i = 1; i <= num; i++){
-        console.log(i);
         someDiv = document.createElement(el);
-        someDiv.textContent =  `Hello I'm div number ` + i;
+        someDiv.textContent = 'Hello World ' + i;
         someDiv.setAttribute('class', 'boxes');
         someDiv.setAttribute('id', 'box' + i);
         parent.appendChild(someDiv);
