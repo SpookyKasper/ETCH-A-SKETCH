dfe2e4784ff4c0b2490f4b98619d720d86487e46
Kasper <kasperbak89@pm.me>
2022-05-07 20:54:17 +0200

    Change the layout using css grid
    The divs where not always square and grid was not consistent

--- eas.js
+++ eas.js
@@ -2,7 +2,7 @@
 function createEl(parent, el, num){
     for(i = 1; i <= num; i++){
         someDiv = document.createElement(el);
-        someDiv.textContent = 'Hello World ' + i;
+        // someDiv.textContent = 'box' + i;
         someDiv.setAttribute('class', 'boxes');
         someDiv.setAttribute('id', 'box' + i);
         parent.appendChild(someDiv);
